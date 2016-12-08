efd6e6f72f713c33eccb54eec0137b029140df7e
mk-anderson11 <matt.anderson11@gmail.com>
2016-12-08 14:00:45 -0700

    6

--- public/javascripts/wolfram.js
+++ public/javascripts/wolfram.js
@@ -6,6 +6,21 @@ function HelloWorld()
 }
 
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+app.controller('myCtrl', function($scope, $http) {
   $scope.firstName="Matthew";
+
+  $scope.Wolfram = function() {
+    alert("Wolfram Function");
+    url = 'http://api.wolframalpha.com/v2/query?input=pi&appid=UKVQX8-68VAQJWH5P'
+    $http.get(url)
+      .then(function(response) {
+      console.log(response);
+    });
+  }
+
+  $http.get('http://api.wolframalpha.com/v2/query?input=pi&appid=UKVQX8-68VAQJWH5P').success(function(data) {
+    console.debug(data);
+  })
+
+
 });
