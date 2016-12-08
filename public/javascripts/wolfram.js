317828b2677a40545002d446a6545f582089db16
mk-anderson11 <matt.anderson11@gmail.com>
2016-12-08 14:29:11 -0700

    10

--- public/javascripts/wolfram.js
+++ public/javascripts/wolfram.js
@@ -11,7 +11,7 @@ app.controller('myCtrl', function($scope, $http) {
   $scope.Output="";
 
   $scope.Wolfram = function() {
-    $scope.Output = $scope.firstName;
+    $scope.Output = 'Hello ' + $scope.firstName + '! Functionality to query WolframAlpha for name statistics coming soon!';
     alert("Wolfram Function");
     url = 'http://api.wolframalpha.com/v2/query?input=pi&appid=UKVQX8-68VAQJWH5P'
     $http.get(url)
